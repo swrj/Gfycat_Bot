e55fb73c62cc11abe5db1ec9bb2416171c254215
Swaraj <srao24@wisc.edu>
2017-07-15 14:38:38 -0500

    Gfycat_Bot.py: Made script more robust
    Added exception handling and switched to subreddit.stream.submissions to prevent the script from missing any posts in /new. Changed user agent to prevent a 429 error

--- src/Gfycat_Bot.py
+++ src/Gfycat_Bot.py
@@ -6,31 +6,36 @@ def bot_login():
     print("Logging in....")
     reddit_instance=praw.Reddit(client_id=config.client_id,
                 client_secret=config.client_secret,
-                user_agent='linux:Gfycat_Link_Fixer:v0.1 (by /u/Gfy_cat_Fixer_Bot)',
+                user_agent='Reddit Gfycat Link Fixer:v0.4 (by /u/Gfy_cat_Fixer_Bot)',
                 username=config.username,
-                password=config.password)
+                password=config.password) 
     print("Logged in.")
     return reddit_instance
 
 def run_bot(reddit_instance):
-    print("Searching for broken links")
-    for submission in reddit_instance.subreddit('RocketLeague+PUBATTLEGROUNDS+Rainbow6+gaming+titanfall').new(limit=20):
+    reddit_instance.domain('gfycat.com')
+    subreddit=reddit_instance.subreddit('rocketleague+overwatch+PUBATTLEGROUNDS')
+    for submission in subreddit.stream.submission(): #using subreddit.stream.submissions to avoid missing new posts
         if "gfycat.com/gifs/detail/" in submission.url:
-            all_comments=submission.comments.list()
+            all_comments=submission.comments
             for items in all_comments: #check to prevent duplicate comments in thread
                 user_name=items.comment
                 if user_name.author=='Gfy_cat_Fixer_Bot':
                     print "Duplicate"
-                    break
+                    continue
             print("Broken link found")
             unique_URL=submission.url[31:] #extracting unique gfycat identifier from bloated link
             fixed_link="https://www.gfycat.com/"+unique_URL
             print(fixed_link)
-            comment="[Direct link to GIF](%s)  [^why ^am ^I ^posting ^this?]" \
+            comment="[Direct link to GIF](%s)  " \
+                    "  [^why ^am ^I ^posting ^this?]" \
                     "(https://gist.github.com/anonymous/e42ca1e302d0dcd4c4c4a949e8a5c682)"%fixed_link
             submission.reply(comment)
-    time.sleep(600) #Bot rests for 10 minutes before running again
 
 reddit_instance=bot_login()
 while True:
-    run_bot(reddit_instance)
+    try:
+        run_bot(reddit_instance)
+    except:
+        continue #restarting bot in case of an exception
+    time.sleep(15)
